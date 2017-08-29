858d6be9c73b3d14636fa77176d74dd091e2a7de
吴奇泽 <wuqize5109@qq.com>
2017-08-29 23:34:03 +0800

    路由映射部分

--- sylfk/__init__.py
+++ sylfk/__init__.py
@@ -6,13 +6,27 @@ from werkzeug.wrappers import Response
 
 from sylfk.wsgi_adapter import wsgi_app
 
+import sylfk.exceptions as exceptions
+
+
+# 处理函数数据结构
+class ExecFunc:
+    def __init__(self, func, func_type, **options):
+        self.func = func # 处理函数
+        self.options = options # 附带参数
+        self.func_type = func_type # 函数类型
+
 
 class SYLFK:
 
     # 实例化方法
-    def __init__(self):
+    def __init__(self, static_folder='static'):
         self.host = '127.0.0.1' # 默认主机
         self.port = 8086 # 默认端口
+        self.url_map = {} # 存放 URL 与 节点（端点）的映射
+        self.static_map = {} # 存放 URL 与静态资源的映射
+        self.function_map = {} # 存放节点（端点）与处理请求函数的映射
+        self.static_folder = static_folder # 静态资源本地存放路径，默认放在应用目录的static文件夹
     
     # 路由
     def dispatch_request(self, request):
@@ -20,12 +34,35 @@ class SYLFK:
 
         # 定义响应报头的 Server 属性
         headers = {
-        'Server': 'Shiyanlou Framework'
+        'Server': 'Simple Framework'
         }
 
         # 回传实现 WSGI 规范的响应体给 WSGI 模块
         return Response('<h1>Hello, Framework</h1>', content_type='text/html', headers=headers, status=status)
-    
+
+    # 路由添加规则
+    def add_url_rule(self, url, func, func_type, endpoint=None, **options):
+
+        # 如果节点未命名， 就使用处理函数名
+        if endpoint is None:
+            endpoint = func.__name__
+
+        # 抛出 URL 已存在异常
+        if url in self.url_map:
+            raise exceptions.URLExistsError
+
+        # 如果类型不是静态资源，并且节点已存在，则抛出节点已存在异常
+        if endpoint in self.function_map and func_type != 'static':
+            raise exceptions.EndpointExistsError
+
+        # 添加 URL 与节点映射
+        self.url_map[url] = endpoint
+
+        # 添加节点与请求处理函数映射
+        self.function_map[endpoint] = ExecFunc(func, func_type, **options)
+
+
+
     # 启动入口
     def run(self, host=None, port=None, **options):
         """
